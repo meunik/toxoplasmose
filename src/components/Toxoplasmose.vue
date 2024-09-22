9a51771c233a680f14fd95535e7e37efc2dd172c
Marcos Paulo <marcostrab@hotmail.com>
2024-09-22 01:15:04 -0300

    Texto refeito, e adicionado gatinhos

--- src/components/Toxoplasmose.vue
+++ src/components/Toxoplasmose.vue
@@ -1,45 +1,59 @@
 <template>
-  <div class="caixa position-relative">
-    <img class="patas-de-gato-img" src="/patas-de-gato.svg" alt="">
-    
-    <h1 class="titulo z-1">Afinal, o que é a Toxoplasmose?</h1>
+  <div class="overflow-y-auto">
+    <div class="caixa position-relative">
+      <img class="patas-de-gato-img" src="/patas-de-gato.svg" alt="">
+      
+      <h1 class="titulo z-1">Toxoplasmose</h1>
 
-    <div class="oQueEToxoplasmose z-lg-1">
-      <p>É uma infecção causada por um parasita chamado Toxoplasma gondii Esse parasita pode infectar muitos tipos de animais e humanos. Essa doença ficou conhecida como "doença do gato" pelo fato de que eles são os principais hospedeiros, mas é importante ressaltar que a infecção não é causada diretamente por eles.</p>
+      <div class="blocoGato position-relative">
+        <div class="oQueEToxoplasmose gatoNaDireita z-lg-1">
+          <p>A Toxoplasmose é uma infecção causada por um parasita chamado Toxoplasma gondii. A doença é contraída principalmente pela ingestão de carne mal cozida ou crua, ou pela ingestão de alimentos e água contaminados ou pelas fezes de gatos contaminados.</p>
 
-      <p>Ela pode ser transmitida pelas fezes dos gatos, alimentos crus ou mal cozidos, e, água e alimentos contaminados. Para a maioria das pessoas, a Toxoplasmose não causa sintomas ou apenas sintomas semelhantes ao da gripe. Porém, quando se trata de mulheres grávidas, o parasita pode passar para o bebê, o que pode causar sérios problemas a saúde dele, como problemas no cérebro e nos olhos. Por isso, é importante que as grávidas tomem alguns cuidados para evitar essa contaminação.</p>
+          <p>É uma doença comum que afeta quase metade da população mundial, porém na maioria dos casos as pessoas não apresentam sintomas e com isso não necessitam de tratamento específico. Os sintomas dessa doença variam de acordo com o sistema imunológico de cada um, mas normalmente os sintomas são semelhantes ao da gripe, mas em pessoas com doenças imunes podem ser graves. </p>
 
-      <p>Sendo assim, é importante ressaltar que NÃO há necessidade das grávidas se afastarem de seus gatos. Apenas sigas as recomendações de prevenção.</p>
-    </div>
-
-    <div class="d-flex flex-lg-row flex-column gap-2 gap-lg-5 px-3 z-1">
-      <div>
-        <h6 class="subTitulo">AQUI ESTÃO ALGUMAS RECOMENDAÇÕES A SEREM SEGUIDAS:</h6>
-
-        <ul class="fw-light d-flex flex-column gap-2">
-          <li>Evitar contato com as fezes de gato. Caso você tenha gatos, peça para que outra pessoa limpe a caixa de areia, ou use luvas e manuseie as fezes com auxílio de pá própria para isto, após a limpeza lave bem as mãos.</li>
-          <li>Não consuma carnes cruas ou mal-passadas.</li>
-          <li>Higienize frutas e legumes antes de consumi-los.</li>
-          <li>Higienize as mãos antes de preparar alimentos e depois de manusear alimentos crus ou entrar em contato com o solo.</li>
-          <li>Evite solo contaminado, ou seja, evitar * jardinagem ou mexer com solo sem luvas.</li>
-          <li>Evite o consumo de leite e produtos lácteos não pasteurizados, pois podem estar contaminados.</li>
-        </ul>
+          <p>Caso a Toxoplasmose seja contraída durante a gravidez pode causar aborto espontâneo ou malformações congênitas e por isso é de extrema importância o acompanhamento pré-natal pois é isso que vai permitir que a doença seja tratada no inicio, antes que o parasita atravesse a placenta e chegue ao feto. É importante ressaltar que não há necessidade das grávidas se afastarem de seus gatos, seguindo as recomendações de prevenções já é o suficiente.</p>
+        </div>
+        <img class="gato2" src="/Gato2.svg" alt="">
       </div>
-      <div>
-        <h6 class="subTitulo">CUIDAR DE SEU ANIMAL TAMBEM É UMA FORMA DE PREVENÇÃO, SEGUINDO ALGUMAS MEDIDAS:</h6>
 
-        <ul class="fw-light d-flex flex-column gap-2">
-          <li>Mantenha a caixa de areia limpa diariamente, e pelo menos uma vez por semana lave-a bem.</li>
-          <li>Descarte as fezes e urina de forma adequada, caso a areia seja biodegradável os dejetos podem ser descartados em vaso sanitário. Mantenha o ambiente ao redor da caixa de areia limpo.</li>
-          <li>Não dê alimentos crus para o gato, dê preferência a alimentos específicos para ele ou cozinhe bem a comida do seu pet.</li>
-          <li>Não deixe-o ter acesso a rua, pois isso os expõe a fontes de infecção, como solo e outros animais infectados. Se possível coloque telas de proteção em janelas, mantendo-o dentro de casa.</li>
-          <li>Leve-o ao veterinário regularmente para exames de rotina e vacinas, para garantir que seu gato esteja saudável e sem fatores de risco para contrair a Toxoplasmose.</li>
-        </ul>
+      <div class="d-flex flex-column gap-3 px-3 z-1">
+        <div>
+          <h6 class="subTitulo">São essas recomendações:</h6>
+
+          <ul class="fw-light d-flex flex-column gap-2">
+            <li>Evite entrar em contato com as fezes de gato. Caso você tenha gatos, peça para que outra pessoa faça a limpeza da caixa de areia, ou use luvas e manuseie as fezes com auxílio de pá própria para isto, após a limpeza lave bem as mãos.</li>
+            <li>Não consuma carnes cruas ou malpassadas, elas podem estar contaminadas. Higienize as mãos antes e depois de preparar alimentos e de manusear alimentos crus.</li>
+            <li>Higienize frutas e legumes.</li>
+            <li>Evite jardinagem ou mexer com solo sem luvas, caso os faça, higienize as mãos em seguida.</li>
+            <li>Evite o consumo de leite e produtos lácteos não pasteurizados, pois podem estar contaminados.</li>
+          </ul>
+
+          <div class="blocoGato position-relative">
+            <img class="gato1" src="/Gato1.svg" alt="">
+            <div class="oQueEToxoplasmose gatoNaEsquerda z-lg-1">
+              <p>Agora você deve estar se perguntando quais são os sintomas da Toxoplasmose nos felinos. Os sintomas dependem tanto do nível da infecção como da forma de transmissão. Muitos felinos não apresentam sintomas, mas alguns sofrem consequências bem sérias. Quando filhotes são infectados pela mãe durante a gestação normalmente apresentam sintomas graves, muitos morrem logo após o nascimento ou alguns dias depois. Aqueles que conseguem sobreviver também podem apresentar lesões graves no fígado ou nos olhos.</p>
+
+              <p>As consequências mais graves ocorrem em felinos que já sofrem com alguma disfunção imunológica, como FIV (conhecida como a AIDS felina) ou Felv (a leucemia felina). Estes costumam apresentar sintomas como:  depressão, anorexia, acúmulo de líquido no abdômen, falta de ar, mucosas amarelas, alterações na região ocular, tremores e convulsão.</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h6 class="subTitulo">Para os gatos também existem algumas medidas de prevenções que podemos seguir, são elas:</h6>
+
+          <ul class="fw-light d-flex flex-column gap-2">
+            <li>Mantenha a caixa de areia limpa diariamente, as lave semanalmente. É importante que o ambiente ao redor da caixa de areia também esteja limpo.</li>
+            <li>Descarte as fezes e urina de forma adequada, caso a areia for biodegradável os dejetos podem ser descartados em vaso sanitário.</li>
+            <li>Não dê alimentos crus para o gato, dê preferência a alimentos específicos para ele ou cozinhe bem a comida do seu pet para matar possíveis parasitas.</li>
+            <li>Evite que o gato saia de casa, pois isso pode expô-los a fontes de infecção, como solo e animais infectados. Se possível coloque telas de proteção nas janelas para que eles não tenham acesso a rua; a castração também auxilia a reduzir as "fugas" em busca de um parceiro(a), e ainda, previne surgimento de outras doenças como tumores de mama, ovário e útero nas fêmeas, e de testículos e próstata nos machos.</li>
+            <li>Leve-o ao veterinário regularmente para a realização de vacinas e exames de rotina, assim tendo certeza de que ele está saudável.</li>
+          </ul>
+        </div>
       </div>
+
+      <h5 class="z-1 text-shadow text-center">E com todas essas informações, deixo aqui o lembrete de que <span class="texto-azul">O GATO NÃO É O VILÃO</span> desta doença! Ela pode ser contraída de diversas fontes! A forma mais comum de infecção é pela ingestão de alimentos e água contaminadas!</h5>
+
+      <img class="patas-de-gato-img-footer" src="/patas-de-gato.svg" alt="">
     </div>
-
-    <h5 class="z-1 text-shadow">Espero que esse panfleto tenha ajudado você a entender melhor a Toxoplasmose, e sessado estigmas associados a essa doença.</h5>
-
-    <img class="patas-de-gato-img-footer" src="/patas-de-gato.svg" alt="">
+    <div class="d-flex justify-content-end px-2"><span><strong>Feito por:</strong> Maria Luiza Peres de Castro Guimarães - Biomedicina | Estácio | Norte Shopping</span></div>
   </div>
 </template>
